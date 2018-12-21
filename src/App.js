84fac70c4c92fad81c2bef02d7995a688b1b9198
zhouyunlong <zhouyunlong@cd.tuan800.com>
2018-12-21 15:25:53 +0800

    上传webpack4

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/**
+ * Author: zhouyunlong@cd.tuan800.com
+ * Date: 2018/12/21
+ * Time: 下午2:58
+ */
+
+
+import React from "react";
+import ReactDOM from "react-dom";
+const App = () => {
+    return (
+        <div>
+            <p>React here!哈哈哈123</p>
+        </div>
+    );
+};
+export default App;
+ReactDOM.render(<App />, document.getElementById("app"));
